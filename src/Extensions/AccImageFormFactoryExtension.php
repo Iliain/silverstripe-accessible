f72dc355e132c46de421fcee53aab1e223f8f3c6
Iliain <drcv251@gmail.com>
2023-07-31 12:31:06 +1000

    FIX: used existing AltText code

--- src/Extensions/AccImageFormFactoryExtension.php
+++ src/Extensions/AccImageFormFactoryExtension.php
@@ -2,11 +2,14 @@
 
 namespace Iliain\Accessible\Extensions;
 
+use SilverStripe\Forms\Tip;
 use SilverStripe\Core\Extension;
 use SilverStripe\Forms\FieldList;
-use SilverStripe\Forms\TextareaField;
-use SilverStripe\AssetAdmin\Forms\FileFormFactory;
+use SilverStripe\Forms\TextField;
 use SilverStripe\Forms\LiteralField;
+use SilverStripe\Forms\TextareaField;
+use SilverStripe\Forms\TippableFieldInterface;
+use SilverStripe\AssetAdmin\Forms\FileFormFactory;
 
 /**
  * Extends ImageFormFactory to add the fields necessary to edit AltText
@@ -25,31 +28,48 @@ class AccImageFormFactoryExtension extends Extension
      */
     public function updateFormFields(FieldList $fields, $controller, $formName, $context)
     {
-        $fields->removeByName(['AltText', 'Caption']);
-
-        $altField = TextareaField::create('AltText', 'Alt Text');
-        $captionField = TextareaField::create('Caption', 'Caption');
-
         if ($this->getFormType($context) === FileFormFactory::TYPE_INSERT_MEDIA) {
+            $fields->removeByName(['AltText', 'Caption']);
             $fields->insertBefore(
                 'PreviewImage',
                 LiteralField::create('AccInstruct', '<p>Click on the \'Details\' button above to edit Alt Text and Captions</p>')
             );
         } else {
+            $altField = TextField::create(
+                'AltText',
+                _t('SilverStripe\\AssetAdmin\\Controller\\AssetAdmin.AltText', 'Alternative text (alt)')
+            );
+            $captionField = TextareaField::create(
+                'Caption', 
+                _t('SilverStripe\\AssetAdmin\\Controller\\AssetAdmin.Caption', 'Caption')
+            );
+            
             $titleField = $fields->fieldByName('Editor.Details.Title');
             if ($titleField) {
                 if ($titleField->isReadonly()) $altField = $altField->performReadonlyTransformation();
                 if ($titleField->isReadonly()) $captionField = $captionField->performReadonlyTransformation();
     
                 $fields->insertAfter(
-                    'LastEdited',
-                    $altField
-                );
-    
-                $fields->insertAfter(
-                    'AltText',
+                    'AttributesDescription',
                     $captionField
                 );
+
+                $fields->insertAfter(
+                    'Caption',
+                    $altField
+                );
+
+                $altTextDescription = _t(
+                    'SilverStripe\\AssetAdmin\\Controller\\AssetAdmin.AltTextTip',
+                    'Description for visitors who are unable to view the image (using screenreaders or ' .
+                    'image blockers). Recommended for images which provide unique context to the content.'
+                );
+
+                if ($altField instanceof TippableFieldInterface) {
+                    $altField->setTip(new Tip($altTextDescription, Tip::IMPORTANCE_LEVELS['HIGH']));
+                } else {
+                    $altField->setDescription($altTextDescription);
+                }
             }
         }
     }
